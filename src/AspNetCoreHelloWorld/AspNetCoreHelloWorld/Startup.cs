a6afaa0d0ace35f8220f5396449021f5b1301d29
Fabiano Nalin <fabiano.nalin@gmail.com>
2019-04-09 20:21:10 -0300

    add 00 - Introdução

---
+++ src/AspNetCoreHelloWorld/AspNetCoreHelloWorld/Startup.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AspNetCoreHelloWorld
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+
+            //app.UseMvc();
+            app.UseMvcWithDefaultRoute();
+
+            app.Use(async (ctx, next) =>
+            {
+                ctx.Response.ContentType = "text/plain;charset=utf-8";
+                await ctx.Response.WriteAsync("Execução Inicial ");
+                await next.Invoke();
+                await ctx.Response.WriteAsync("Execução final ");
+            });
+
+            app.Map("/map1", HandleMap1);
+            app.Map("/map2", HandleMap2);
+
+            // app.MapWhen(ctx => true, HandleMapWhen);
+            app.MapWhen(ctx => false, HandleMapWhen);
+
+
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync("Bateu no final ");
+            });
+
+
+        }
+
+        private void HandleMapWhen(IApplicationBuilder app)
+        {
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync("Bateu no run do MapWhen ");
+            });
+        }
+
+        private void HandleMap2(IApplicationBuilder app)
+        {
+            app.Use(async (ctx, next) =>
+            {
+                await ctx.Response.WriteAsync("Execução Inicial do Map2 ");
+                await next.Invoke();
+                await ctx.Response.WriteAsync("Execução Final do Map2 ");
+            });
+
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync("Bateu no run do Map2 ");
+            });
+        }
+
+        private void HandleMap1(IApplicationBuilder app)
+        {
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync("Bateu no run do Map1 ");
+            });
+        }
+    }
+}
---
+++ src/AspNetCoreHelloWorld/AspNetCoreHelloWorld/Teste.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetCoreHelloWorld
+{
+    [Controller]
+    public class Teste
+    // public class TesteController
+    // public class Teste: Controller
+    {
+
+        public string Ping() => "Pong";
+
+    }
+}
